0bb9bb8e74e06d2adcb1e13e54ad8f55d11b678d
Orrijinal-Creator <haroldjirrodraco@gmail.com>
2026-03-05 22:20:35 +0800

    Update main.js

--- main.js
+++ main.js
@@ -10,3 +10,29 @@ function sendMessage(event){
 event.preventDefault();
 alert("Message sent successfully!");
 }
+
+let slideIndex = 0;
+
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index){
+
+slides.forEach(slide => slide.classList.remove("active"));
+
+slideIndex = (index + slides.length) % slides.length;
+
+slides[slideIndex].classList.add("active");
+
+}
+
+function changeSlide(direction){
+
+showSlide(slideIndex + direction);
+
+}
+
+/* AUTO SLIDESHOW */
+
+setInterval(()=>{
+changeSlide(1);
+},4000);
